e9eb63df03e4b8dee7358e7f760ade1946dbbeca
BoutainaD <boutaina.didioui@gmail.com>
2018-11-12 13:26:21 +0100

    update

--- server.R
+++ server.R
@@ -3,7 +3,6 @@ library(DBI)
 library(RMySQL)
 library(shiny)
 library(shinydashboard)
-library(jsonlite)
 library(RMySQL)
 library(leaflet)
 
@@ -83,6 +82,32 @@ server<-function(input, output) {
       addLegend(pal = couleurs, values = ~nombre, opacity = 0.9)
     
     
+  })
+  
+  output$progressBox <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple"
+    )
+  })
+  output$approvalBox <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  output$progressBox2 <- renderInfoBox({
+    infoBox(
+      "Progress", paste0(25 + input$count, "%"), icon = icon("list"),
+      color = "purple", fill = TRUE
+    )
+  })
+  
+  output$approvalBox2 <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow", fill = TRUE
+    )
   })
   
 }
